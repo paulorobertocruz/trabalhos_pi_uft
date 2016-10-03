e3de780e21cacae1e8e58aeaf81878230ddecb14
Paulo Roberto Cruz França <paulo.cruz9@gmail.com>
2016-10-03 18:26:04 -0300

    Code Refector Mudanças drasticas no implementação do labelling

--- labelling.py
+++ labelling.py
@@ -2,11 +2,10 @@ from PIL import Image
 from random import randint
 
 def blackAndWhite(image):
-    size = image.size
-    new_image = Image.new("RGB",(size[0],size[1]),(0,0,0))
-    for i in range(size[0]):
-        for j in range(size[1]):
-            p = image.getpixel(i,j)
+    new_image = Image.new("RGB",image.size,(0,0,0))
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            p = image.getpixel( (i,j) )
             m = (p[0] + p[1] + p[2])/3
             if m > 127:
                 new_image.putpixel((i,j),(255,255,255))
@@ -15,33 +14,49 @@ def blackAndWhite(image):
     return new_image
 
 def labelling(image):
-    size = image.size
     binary_image = blackAndWhite(image)
-    new_image = Image.new("RGB",(size[0],size[1]),(0,0,0))
-    
-    for i in range(size[0]):
-        for j in range(size[1]):
-            p = binary_image.getpixel(i,j)
-            if p == (255,255,255):
-                if (p[0]-1,p[1]) = (255,255,255) and (p[0],p[1]-1) = (255,255,255):
-                    r = new_image.getpixel(i-1,j)
-                    s = new_image.getpixel(i,j-1)
-                    if r = s:
-                        new_image.putpixel((i,j),(r[0],r[1],r[2]))
-                    else:
-                        new_image.putpixel((i,j),(r[0],r[1],r[2]))
-                        new_image.putpixel((i,j-1),(r[0],r[1],r[2]))
-                elif (p[0]-1,p[1]) = (255,255,255):
-                    r = new_image.getpixel(i-1,j)
-                    new_image.putpixel((i,j),(r[0],r[1],r[2]))
-                elif (p[0],p[1]-1) = (255,255,255):
-                    s = new_image.getpixel(i,j-1)
-                    new_image.putpixel( (i,j),(s[0],s[1],s[2]) )
-                else:
-                    r = random.randint(0,255)
-                    g = random.randint(0,255)
-                    b = random.randint(0,255)
-                    new_image.putpixel((i,j),(r,g,b))
+    new_image = Image.new("RGB", image.size, (255,255,255))
+    labels = [(100, 100, 100), (0, 0, 0), (0, 255, 0), (255, 0, 0), (0, 0, 255), (255, 200, 100)]
+    next_label = 0
+    similar_labels = [i for i in range(len(labels))]
+    image_labels = [ [-1 for _ in range(image.size[1])] for _ in range(image.size[0])]
+    #in_range
+    def inr(x, y):
+        if x >= 0 and x < image.size[0] and y >= 0 and y < image.size[1]:
+            return True
+        return False
+
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            p = binary_image.getpixel( (i,j) )
+            if p == (0,0,0):
+                r = binary_image.getpixel( (i-1,j) )
+                s = binary_image.getpixel( (i,j-1) )
+                if r == (255,255,255) and s == (255,255,255):
+                    image_labels[i][j] = next_label
+                    next_label += 1
+                elif r == (0, 0, 0) and s != (0, 0, 0):
+                    image_labels[i][j] = image_labels[i-1][j]
+                elif s == (0, 0, 0) and r != (0, 0, 0):
+                    image_labels[i][j] = image_labels[i][j-1]
+                elif s == (0, 0, 0) and r == (0, 0, 0) and image_labels[i][j-1] == image_labels[i-1][j]:
+                    image_labels[i][j] = image_labels[i][j-1]
+                elif s == (0, 0, 0) and r == (0, 0, 0) and image_labels[i][j-1] != image_labels[i-1][j]:
+                    image_labels[i][j] = image_labels[i][j-1]
+                    similar_labels[image_labels[i][j-1]] = image_labels[i-1][j]
+
+    #junta labels similar_labels
+    for i in range(len(similar_labels)):
+        if similar_labels[i] != i:
+            for j in range(len(similar_labels)):
+                if similar_labels[j] == i:
+                    similar_labels[j] = similar_labels[i]
+
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            new_image.putpixel((i,j), labels[ similar_labels[image_labels[i][j]] ])
+    #preenche a imagem de labels
+    return new_image
 
 
 
@@ -51,7 +66,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Faltando path/to/image.png")
         exit()
-
-    image = Image.new(sys.argv[1])
-
-    labelling(image).show()
+    print("tretas")
+    image_path = sys.argv[1]
+    image = Image.open(image_path)
+    image.save("labelling/"+image.filename.split('/')[-1])
+    labelling(image).save("labelling/resultado.png")
