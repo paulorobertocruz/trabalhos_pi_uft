475edd0694f4b92f04df77a5a3427a09ea876ee7
paulorobertocruz <paulo.cruz9@gmail.com>
2016-10-03 00:14:01 -0300

    Add escala e multiplicação implementação de duas funções locais escala por fator e multiplicação de imagens

--- arithmetics.py
+++ arithmetics.py
@@ -1,4 +1,4 @@
-from PIL import image
+from PIL import Image
 from interpolation import b_interpolacao
 
 def even(image1, image2):
@@ -20,7 +20,27 @@ def soma(image1, image2):
 
     return nimage
 
+def multiplicacao(image_a, image_b):
+    for i in range(image_a.size[0]):
+        for j in range(image_a.size[1]):
+            p = image_a.getpixel((i,j))
+            b = image_b.getpixel((i,j))
+            pr = max(0, min(int(p[0] * b[0]), 255))
+            pg = max(0, min(int(p[1] * b[1]), 255))
+            pb = max(0, min(int(p[2] * b[2]), 255))
+            image_a.putpixel((i,j), (pr, pg, pb))
+    return image
 
+def escala(image, fator):
+    fator = float(fator)
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            p = image.getpixel((i,j))
+            pr = max(0, min(int(p[0] * fator), 255))
+            pg = max(0, min(int(p[1] * fator), 255))
+            pb = max(0, min(int(p[2] * fator), 255))
+            image.putpixel((i,j), (pr, pg, pb))
+    return image
 
 def arith(image1,image2,op):
     n_img = Image.new("RGB", (image1.size[0],image1.size[1]), (0,0,0))
@@ -37,7 +57,29 @@ def arith(image1,image2,op):
                 n_img.putpixel((i,j),(p1[0]*p2[0],p1[1]*p2[1],p1[2]*p2[2]))
             elif op == "/":
                 n_img.putpixel((i,j),(int(p1[0]/p2[0]),int(p1[1]/p2[1]),int(p1[2]/p2[2])))
-            else
+            else:
                 n_img.putpixel((i,j),(int((p1[0]+p2[0])/2),int((p1[1]*p2[1])/2),int((p1[2]*p2[2])/2)))
 
     return n_img
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("faltando argumentos")
+        exit()
+
+    operation = sys.argv[1]
+    options = sys.argv[2]
+    image_path = sys.argv[3]
+
+    image = Image.open(image_path)
+    image.show()
+
+    if operation == "escala":
+        escala(image, options).show()
+    elif operation == "soma":
+        image_b = Image.open(options)
+        soma(image, image_b).show()
+    elif operation == "multiplicacao":
+        image_b = Image.open(options)
+        multiplicacao(image, image_b).show()
